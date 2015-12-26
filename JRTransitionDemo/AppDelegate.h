3a97e4006cd3c57639ce745828a01812f8288cab
王潇 <23601145@qq.com>
2015-12-26 09:58:54 +0800

    Initial Commit

---
+++ JRTransitionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JRTransitionDemo
+//
+//  Created by wxiao on 15/12/26.
+//  Copyright © 2015年 wxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JRTransitionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JRTransitionDemo
+//
+//  Created by wxiao on 15/12/26.
+//  Copyright © 2015年 wxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
